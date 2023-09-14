141ec7d1032d781394b2d17dcb2bbd56da82add4
farhan khan <fk828633@gmail.com>
2023-09-15 02:16:16 +0530

    Making the Details page work properly

--- pages/[meetupid]/index.js
+++ pages/[meetupid]/index.js
@@ -1,47 +1,64 @@
+import { MongoClient, ObjectId } from "mongodb";
 import MeetupDetails from "../../components/meetups/MeetupDetails";
 
-function MeetupDetailsPage(){
-    return <MeetupDetails 
-        image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRCIq1WFIqckI69eGZ67ugLdfxchy96eLR7w&usqp=CAU" 
-        id= "M2"
-        title = "A first Meetup"
-        address = 'Some address 5, 12345 some city'
-        description = "This is first meetup"
-        />
+function MeetupDetailsPage(props) {
+  return (
+    <MeetupDetails
+      image={props.meetupData.image}
+      id={props.meetupData.id}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
+    />
+  );
 }
 
+export async function getStaticPaths() {
+  const clinet = await MongoClient.connect(
+    "mongodb+srv://Farhan:bH5ot0llCUyOuCvB@cluster0.euraxek.mongodb.net/meetup?retryWrites=true&w=majority"
+  );
+  const db = clinet.db();
 
-export async function getStaticPaths(){
-    return{
-        fallback:false,
-        paths:[
-            {params:{
-                meetupid: "M1",
-            }},
-            {params:{
-                meetupid: "M2",
-            }},
-            {params:{
-                meetupid: "M3",
-            }},
-        ]
-    }
+  const meetupsCollections = db.collection("meetup");
+
+  const meetups = await meetupsCollections.find({}, { _id: 1 }).toArray();
+
+  clinet.close();
+
+  return {
+    fallback: false,
+    paths: meetups.map((meetups) => ({
+      params: { meetupid: meetups._id.toString() },
+    })),
+  };
 }
 
-export async function getStaticProps(context){
-    const meetupid = context.params.meetupid;
+export async function getStaticProps(context) {
+  const meetupid = context.params.meetupid;
 
-    return {
-        props:{
-            meetupData:{
-                image : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRCIq1WFIqckI69eGZ67ugLdfxchy96eLR7w&usqp=CAU" ,
-                meetupid: meetupid,
-                title:"A first Meetup",
-                address:'Some address 5, 12345 some city',
-                description:"This is first meetup"
-            }
-        }
-    }
+  const clinet = await MongoClient.connect(
+    "mongodb+srv://Farhan:bH5ot0llCUyOuCvB@cluster0.euraxek.mongodb.net/meetup?retryWrites=true&w=majority"
+  );
+  const db = clinet.db();
+
+  const meetupsCollections = db.collection("meetup");
+
+  const selectedMeetup = await meetupsCollections.findOne({_id:new ObjectId(meetupid)})
+  clinet.close();
+
+
+
+  return {
+    props: {
+      meetupData: {
+        image:selectedMeetup.image,
+        meetupid: selectedMeetup._id.toString(),
+        title: selectedMeetup.title,
+        address: selectedMeetup.address,
+        description: selectedMeetup.description,
+      },
+    },
+  };
 }
 
 export default MeetupDetailsPage;
